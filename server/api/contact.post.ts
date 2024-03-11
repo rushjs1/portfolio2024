54dc00be0446ba1ccf7b800687229732f97d0de6
rushjs1 <rushjsdev@gmail.com>
2024-03-11 16:59:23 -0400

    fix: oops

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -1,7 +1,6 @@
 export default defineEventHandler(async (event) => {
-  //const { webhookUrl } = useRuntimeConfig(event)
+  const { webhookUrl } = useRuntimeConfig(event)
   const body = await readBody(event)
-  console.log(body)
 
   const webhookBody = {
     embeds: [{
@@ -13,11 +12,10 @@ export default defineEventHandler(async (event) => {
       ]
     }]
   }
-  console.log("foo")
+  console.log(webhookUrl)
   
   try {
-    console.log("bar")
-    await $fetch('https://discord.com/api/webhooks/1216523466992259132/Qj9nl3iZLFdwec3OiYBLHHXibV3jVZ3uMI803A_1AJTRUxPs8KL8OGx1KzkPGJKflg7_', {
+    await $fetch(webhookUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -25,15 +23,11 @@ export default defineEventHandler(async (event) => {
       body: JSON.stringify(webhookBody)
     })
   } catch(e){
-    console.log('baz')
-    console.log(e)
-
     throw createError({
       statusCode: 500,
       statusMessage: 'There was a server error.'
     })
   }
-    console.log('argghg')
 
   return { 
     ok: true, 
