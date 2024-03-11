ef7f71e56d7a78e46061b81d7ce7688511caa829
rushjs1 <rushjsdev@gmail.com>
2024-03-11 14:51:34 -0400

    fix: contact form attempt

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -13,13 +13,20 @@ export default defineEventHandler(async (event) => {
     }]
   }
   
-  $fetch(webhookUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: webhookBody
-  })
+  try {
+    $fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: webhookBody
+    })
+  } catch(e){
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'There was a server error.'
+    })
+  }
 
   return { 
     ok: true, 
